c41dfc19230af510efbd93891b1d0eba55d42087
BangNguyen2612 <bang.nguyen2612@gmail.com>
2022-01-17 17:31:14 +0700

    culi

---
+++ recognition/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from recognition import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('facecam_feed', views.facecam_feed, name='facecam_feed'),
+    # path('success', views.success, name='success'),
+    path('detectImage', views.detectImage, name='detectImage'),
+    path('train_model', views.Train, name='train_model'),
+
+    ]
